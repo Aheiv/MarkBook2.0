065efeaa56dc1d6bf1936f2a408273ec594f08d9
DaniilF2323@outlook.com <DaniilF2323@outlook.com>
2023-12-16 23:35:03 +0300

    Добавьте файлы проекта.

---
+++ MarkBook/Data/Database/Model/Schedule.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarkBook.Data.Database.Model
+{
+
+    [Table("Schedule")]
+    public partial class Schedule
+    {
+        [Key]
+        public int id_schedule { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        public string weekday { get; set; }
+
+        public int order { get; set; }
+
+        public virtual Grade Grade { get; set; }
+
+        public virtual Subject Subject { get; set; }
+
+        public virtual Teacher Teacher { get; set; }
+    }
+}
